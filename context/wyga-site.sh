827c7a1db93c4d2cf4af0442866b7e065535de9f
Robert Socha <socha@socha.it>
2024-08-31 18:53:09 +0200

    wyga-site-traefik

--- context/wyga-site.sh
+++ context/wyga-site.sh
@@ -8,13 +8,16 @@ then
     --log.level="${wyga_log_level}" \
     --accesslog="${wyga_accesslog}" \
     --providers.docker \
-    --providers.docker.network=wyga-site \
-    '--providers.docker.defaultRule={{ $item := 0 }}{{ range $host := mustRegexSplit "," (index .Labels "wyga.expose") -1 }}{{ $host = trim $host }}{{ if ne $host "" }}{{ $name := regexFind `^([^.]+)` $host }}{{ $domain := regexFind `\.(.+)$` $host }}{{ $domain = trimAll "." $domain }}{{ if eq $domain "" }}{{ $domain = "wyga.site" }}{{ end }}{{ $wildcard := false }}{{ if hasSuffix "*" $name }}{{ $wildcard = true }}{{ end }}{{ $name = trimSuffix "*" $name }}{{ if ne $name "" }}{{ if ne $item 0 }} || {{ end }}Host(`{{ $name }}.{{ $domain }}`){{ if $wildcard }} || HostRegexp(`{{ $name }}-{rx:[0-9a-z][0-9a-z-]*}.{{ $domain }}`){{ end }}{{ $item = 1 }}{{ end }}{{ end }}{{ end }}' \
-    '--providers.docker.constraints=LabelRegex(`traefik.enable`,`(?i)^(yes|true|on)$`) || LabelRegex(`wyga.gateway`,`(?i)^(yes|true|on|enable|allow|activate|active|use)$`) || LabelRegex(`wyga.expose`,`.+`)' \
+    --providers.docker.network=gateway \
+    '--providers.docker.defaultRule={{ $item := 0 }}{{ range $host := mustRegexSplit "," (index .Labels "expose.vhost") -1 }}{{ $host = trim $host }}{{ if contains "." $host }}{{ $name := regexFind `^([^.]+)` $host }}{{ $domain := regexFind `\.(.+)$` $host }}{{ $domain = trimAll "." $domain }}{{ $wildcard := false }}{{ if hasSuffix "*" $name }}{{ $wildcard = true }}{{ end }}{{ $name = trimSuffix "*" $name }}{{ if ne $name "" }}{{ if ne $item 0 }} || {{ end }}Host(`{{ $name }}.{{ $domain }}`){{ if $wildcard }} || HostRegexp(`(?i)^{{ $name }}-[0-9a-z][0-9a-z-]*.{{ $domain }}$`){{ end }}{{ $item = 1 }}{{ end }}{{ end }}{{ end }}' \
+    '--providers.docker.constraints=LabelRegex(`traefik.enable`,`(?i)^(yes|true|on)$`) || LabelRegex(`expose.gateway`,`(?i)^(yes|true|on|enable|allow|activate|active|use)$`) || LabelRegex(`expose.vhost`,`.+`)' \
     --providers.file.directory=/config/runtime \
     --providers.file.watch=false \
     --entrypoints.https.address=:443 \
     --entrypoints.https.http.tls=true \
+    --entrypoints.http.address=:80 \
+    --entrypoints.http.http.redirections.entrypoint.to=https \
+    --entrypoints.http.http.redirections.entrypoint.scheme=https \
     --entrypoints.https.forwardedheaders.insecure=true \
     --serversTransport.insecureSkipVerify=true \
     --api.dashboard=true \
